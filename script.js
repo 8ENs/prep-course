8cf09c36808d41c6a46ada9417b4ea7f7a5dd510
8ENs <ben@coderush.ca>
2015-04-25 13:07:17 -0700

    Assignment: Recipe + JS

--- script.js
+++ script.js
@@ -1,3 +1,22 @@
 $(document).ready(function() {
   console.log("Script included!");
+  
+  $('a.toggler').click(function() {
+    $(this).toggleClass('off');
+    $('img').toggle('slow');
+  });
+  
+  $('#directions').accordion();
+
+  $('li').click(function() {
+    $(this).toggleClass('checked');    
+  });
+
+  $('li').hover(
+    function() {
+      $(this).append($("<span> <em>(click to cross on/off)</em></span>"));
+    }, function() {
+      $(this).find("span:last").remove();
+    }
+  );
 });
